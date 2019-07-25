4ccabbd8daeba9cbd7be78da7163396daee1a7ec
= <wanirou14titech@gmail.com>
2019-07-25 16:50:39 +0900

    検索条件表示機能追加

---
+++ app/helpers/chart/puc_count_helper.rb
@@ -0,0 +1,36 @@
+module Chart::PucCountHelper
+  def display_search_conditions(conditions)
+    tag.ul class: 'uk-margin-remove-top uk-list uk-list-bullet' do
+      if conditions.present?
+        safe_join(
+          conditions.map do |condition|
+            tag.li(
+              info_str(condition),
+              class: 'uk-margin-remove-top'
+            )
+          end
+        )
+      else
+        tag.li '全譜面'
+      end
+    end
+  end
+
+  private
+
+  def info_str(condition)
+    attr_name = condition.a.first.attr_name
+    val = condition.value
+
+    val_str = case attr_name
+      when 'level'
+        val.join(', ')
+      when 'difficult'
+        val.map { |v| Chart.difficult.find_value v }.join(', ')
+      when 'musicname'
+        "『#{val}』 を含む"
+      end
+
+    Chart.human_attribute_name(attr_name) + ' ・・・ ' + val_str
+  end
+end
