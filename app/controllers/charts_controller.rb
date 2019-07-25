d08fc41f5b6d6bffaad0e2908bf20870a4a6da4e
= <wanirou14titech@gmail.com>
2019-07-25 19:04:29 +0900

    デフォルトのソート順を指定

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,9 +4,12 @@ class ChartsController < ApplicationController
   def puc_count
     @chart_update_master = ChartUpdateMaster.first
 
+    # TODO: ストロングパラメータ実装
     @q = Chart.ransack(params[:q])
+    @s = params[:q][:s] if params[:q].present?
     @charts = @q.result(distinct: true)
                 .page(params[:page])
                 .per(PER_PAGE)
+    @charts = @charts.order(id: :desc) unless @s.present?
   end
 end
