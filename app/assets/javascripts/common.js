4d8fddcf5dced1bb355a648d1121a8fe07a4f549
= <wanirou14titech@gmail.com>
2019-07-25 15:04:52 +0900

    リセットボタン追加

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,5 +1,11 @@
 $(function() {
-  $('.js-clear-text').on('click', function() {
-    console.log('hoge');
+  $('.js-reset-checkbox').on('click', function() {
+    var targets = $(this).parents('.js-reset-container').find('.js-reset-target');
+    targets.prop('checked', false);
+  });
+
+  $('.js-reset-text').on('click', function() {
+    var targets = $(this).parents('.js-reset-container').find('.js-reset-target');
+    targets.val('');
   });
 });
