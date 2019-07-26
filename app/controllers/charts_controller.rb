0c45cd18da1411de0bbc8e5afe913a5f2b9f921e
= <wanirou14titech@gmail.com>
2019-07-26 16:08:02 +0900

    微修正

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,15 +2,21 @@ class ChartsController < ApplicationController
   PER_PAGE = 100
 
   def puc_count
+    # 譜面更新日マスタ
     @chart_update_master = ChartUpdateMaster.first
 
-    # TODO: ストロングパラメータ実装
+    # ransack検索クエリ
     @q = Chart.ransack(params[:q])
+    # ransackソートクエリ
     @s = params[:q][:s] if params[:q].present?
+
+    # 譜面データ
     @charts = @q.result(distinct: true)
                 .page(params[:page])
                 .per(PER_PAGE)
     @charts = @charts.order(id: :desc) unless @s.present?
+
+    # PC/SPでページネーションの出力ページ数を変更 PC:5, SP: 2
     @window_size = browser.device.mobile? ? 2 : 5
   end
 end
