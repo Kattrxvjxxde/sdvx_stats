70df19bb29a9f03361a3d5fefd11398e0f72a3be
= <wanirou14titech@gmail.com>
2019-07-26 15:56:08 +0900

    ページネーションのフェードアウト実装

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,11 +1,26 @@
 $(function() {
+  // チェックボックスリセット
   $('.js-reset-checkbox').on('click', function() {
     var targets = $(this).parents('.js-reset-container').find('.js-reset-target');
     targets.prop('checked', false);
   });
 
+  // テキストフィールドリセット
   $('.js-reset-text').on('click', function() {
     var targets = $(this).parents('.js-reset-container').find('.js-reset-target');
     targets.val('');
+  });
+
+
+  // 最下部で上部ページネーションを隠す
+  $(window).on("scroll", function() {
+    var scrollHeight = $(document).height();
+    var scrollPosition = $(window).height() + $(window).scrollTop();
+    if ((scrollHeight - scrollPosition) / scrollHeight <= 0.01) {
+      // when scroll to bottom of the page
+      $('.js-paginate').fadeOut();
+    } else {
+      $('.js-paginate').fadeIn();
+    }
   });
 });
