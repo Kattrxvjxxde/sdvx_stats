b183d6a582b36269521d3a352537e0d7669d022b
= <wanirou14titech@gmail.com>
2019-07-29 17:51:50 +0900

    もろもろデザイン変更

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,13 +2,19 @@ class ChartsController < ApplicationController
   PER_PAGE = 100
 
   def puc_count
-    # 譜面更新日マスタ
-    @chart_update_master = ChartUpdateMaster.first
+    # 譜面更新日
+    @chart_update_date = ChartUpdateMaster.first.update_date
 
     # ransack検索クエリ
     @q = Chart.ransack(params[:q])
     # ransackソートクエリ
     @s = params[:q][:s] if params[:q].present?
+    # ransack検索クエリ情報
+    @q_conditions = @q.conditions.each_with_object({}) do |condition, hash|
+      hash[condition.a.first.attr_name] = condition.value
+    end
+    @q_conditions['level'] = 'ALL' unless @q_conditions.key?('level')
+    @q_conditions['difficult'] = Chart.difficult.values unless @q_conditions.key?('difficult')
 
     # 譜面データ
     @charts = @q.result(distinct: true)
