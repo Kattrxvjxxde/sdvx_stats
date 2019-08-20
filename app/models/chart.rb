ec900a4e5805893e630f6dc1888843ad92671bdc
wanirou <wanirou14titech@gmail.com>
2019-08-20 22:32:14 +0900

    Chartモデルのリレーション設定修正

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,5 +1,5 @@
 class Chart < ApplicationRecord
-  belongs_to :music
+  belongs_to :music, optional: true
 
   extend Enumerize
   enumerize :difficult, in: { NOV: 0, ADV: 1, EXH: 2, MXM: 3, INF: 4, GRV: 5, HVN: 6, VVD: 7 }
